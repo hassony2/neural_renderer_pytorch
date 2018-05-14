6ac58127ac3db9d0a8149c54d2a77ac808c0b4ea
Yana <yanahasson@gmail.com>
2018-05-14 15:45:36 +0200

    Add original example_2

--- examples/example_2_th.py
+++ examples/example_2_th.py
@@ -10,46 +10,38 @@ import chainer
 import chainer.functions as cf
 import numpy as np
 import scipy.misc
+import torch
 import tqdm
 
-from neurender.renderer import Renderer
+from neurender.get_points_from_angles import get_points_from_angles_th
+from neurender.renderer_th import Renderer
 from neurender.load_obj import load_obj
-from neurender.get_points_from_angles import get_points_from_angles
 
 
-class Model(chainer.Link):
+class Model(torch.nn.Module):
     def __init__(self, filename_obj, filename_ref):
-        super(Model, self).__init__()
+        super().__init__()
 
-        with self.init_scope():
-            # load .obj
-            vertices, faces = load_obj(filename_obj)
-            self.vertices = chainer.Parameter(vertices[None, :, :])
-            self.faces = faces[None, :, :]
+        vertices, faces = load_obj(filename_obj)
+        self.vertices = vertices
+        self.faces = faces
 
-            # create textures
-            texture_size = 2
-            textures = np.ones((1, self.faces.shape[1], texture_size,
-                                texture_size, texture_size, 3), 'float32')
-            self.textures = textures
+        # create textures
+        texture_size = 2
+        textures = np.ones((1, self.faces.shape[1], texture_size, texture_size,
+                            texture_size, 3), 'float32')
+        self.textures = textures
 
-            # load reference image
-            self.image_ref = scipy.misc.imread(filename_ref).astype(
-                'float32').mean(-1) / 255.
+        # load reference image
+        self.image_ref = torch.Tensor(
+            scipy.misc.imread(filename_ref).astype('float32').mean(-1) / 255.)
 
-            # setup renderer
-            renderer = Renderer()
-            renderer.anti_aliasing = False
-            self.renderer = renderer
+        # setup renderer
+        renderer = Renderer()
+        self.renderer = renderer
 
-    def to_gpu(self, device=None):
-        super(Model, self).to_gpu(device)
-        self.faces = chainer.cuda.to_gpu(self.faces, device)
-        self.textures = chainer.cuda.to_gpu(self.textures, device)
-        self.image_ref = chainer.cuda.to_gpu(self.image_ref, device)
-
-    def __call__(self):
-        self.renderer.eye = get_points_from_angles(2.732, 0, 90)
+    def forward(self):
+        self.renderer.eye = get_points_from_angles_th(2.732, 0, 90)
         image = self.renderer.render_silhouettes(self.vertices, self.faces)
         loss = cf.sum(cf.square(image - self.image_ref[None, :, :]))
         return loss
@@ -68,10 +60,7 @@ def make_gif(working_directory, filename):
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-io',
-        '--filename_obj',
-        type=str,
-        default='./examples/data/teapot.obj')
+        '-io', '--filename_obj', type=str, default='examples/data/teapot.obj')
     parser.add_argument(
         '-ir',
         '--filename_ref',
@@ -91,20 +80,37 @@ def run():
     args = parser.parse_args()
     working_directory = os.path.dirname(args.filename_output_result)
 
-    model = Model(args.filename_obj, args.filename_ref)
-    model.to_gpu()
+    vertices, faces = load_obj(args.filename_obj)
+    vertices = torch.Tensor(vertices).cuda().unsqueeze(0)
+    vertices.requires_grad = True
+    faces = torch.Tensor(faces).cuda().unsqueeze(0)
+    faces.requires_grad = True
 
-    optimizer = chainer.optimizers.Adam()
-    optimizer.setup(model)
+    # create textures
+    texture_size = 2
+    textures = faces.new_full(
+        (1, faces.shape[1], texture_size, texture_size, texture_size, 3),
+        fill_value=1)
+
+    # load reference image
+    image_ref = torch.Tensor(
+        scipy.misc.imread(args.filename_ref).astype('float32').mean(-1) / 255.)
+    image_ref = image_ref.cuda()
+
+    # setup renderer
+    renderer = Renderer()
+
+    optimizer = torch.optim.Adam([vertices, faces])
     loop = tqdm.tqdm(range(300))
     for i in loop:
         loop.set_description('Optimizing')
-        optimizer.target.cleargrads()
-        loss = model()
+        optimizer.zero_grad()
+        renderer.eye = get_points_from_angles_th(2.732, 0, 90)
+        images = renderer.render_silhouettes(vertices, faces)
+        loss = torch.sum((images - image_ref)**2)
         loss.backward()
-        optimizer.update()
-        images = model.renderer.render_silhouettes(model.vertices, model.faces)
-        image = images.data.get()[0]
+        optimizer.step()
+        image = images.detach().cpu().numpy()[0]
         scipy.misc.toimage(
             image, cmin=0,
             cmax=1).save('%s/_tmp_%04d.png' % (working_directory, i))
@@ -114,10 +120,9 @@ def run():
     loop = tqdm.tqdm(range(0, 360, 4))
     for num, azimuth in enumerate(loop):
         loop.set_description('Drawing')
-        model.renderer.eye = get_points_from_angles(2.732, 0, azimuth)
-        images = model.renderer.render(model.vertices, model.faces,
-                                       model.textures)
-        image = images.data.get()[0].transpose((1, 2, 0))
+        renderer.eye = get_points_from_angles_th(2.732, 0, azimuth)
+        images = renderer.render(vertices, faces, textures)
+        image = images.detach().cpu().numpy()[0].transpose((1, 2, 0))
         scipy.misc.toimage(
             image, cmin=0,
             cmax=1).save('%s/_tmp_%04d.png' % (working_directory, num))
